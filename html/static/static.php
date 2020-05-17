97fc0029e152ec6f5ee60c96edd7c7c6e460bf05
Kotaro Suzuki <kotarosz727@gmail.com>
2020-05-17 16:41:41 +0900

    遅延静的束縛

--- html/static/static.php
+++ html/static/static.php
@@ -2,33 +2,33 @@
 
 abstract class Smartphone
 {
-    protected static $price;
     protected static $class = __CLASS__;
-    
-    
-    public static function getPrice()
-    {
-        // return number_format(self::$price);
-        return number_format(static::$price);
-    }
+    protected static $price;
     
     public static function getClass()
     {
         // return self::$class;
         return static::$class;
     }
+
+    public static function getPrice()
+    {
+        // return number_format(self::$price);
+        return number_format(static::$price);
+    }
+    
 }
 
 class iPhone11 extends Smartphone
 {
-    protected static $price = 80000;
     protected static $class = __CLASS__;
+    protected static $price = 80000;
 }
 
 class iPhone11Pro extends Smartphone
 {
-    protected static $price = 100000;
     protected static $class = __CLASS__;
+    protected static $price = 100000;
 }
 
 echo iPhone11::getPrice()."\n";
